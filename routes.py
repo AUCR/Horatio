afbd920574e76932edb52ca5914b6899274d4562
Wyatt Roersma <wyattroersma@gmail.com>
2018-05-12 19:17:04 -0600

    update nvabar and routes to match aucr 0.3.1.6 format

--- routes.py
+++ routes.py
@@ -7,7 +7,7 @@ from app.plugins.tasks.routes import tasks_page
 from app.plugins.Horatio.forms import CreateCase
 from app.plugins.Horatio.globals import AVAILABLE_CHOICES
 from app.plugins.Horatio.models import Cases
-from app.plugins.auth.utils import get_groups
+from app.plugins.auth.utils import get_group_permission_navbar
 
 cases = Blueprint('cases', __name__, template_folder='templates')
 
@@ -17,11 +17,10 @@ cases = Blueprint('cases', __name__, template_folder='templates')
 def cases_plugin_route():
     """the tasks function returns the plugin framework for the yara_plugin default task view"""
     # TODO show current cases in the database
-    current_user_groups = get_groups()
     page = request.args.get('page', 1, type=int)
     case_list = Cases.query.all()
     return render_template('cases.html', title='Cases Plugin',
-                           page=page, render_current_user_groups=current_user_groups, case_list=case_list)
+                           page=page, current_user_navbar=get_group_permission_navbar(), case_list=case_list)
 
 
 @tasks_page.route('/create', methods=['GET', 'POST'])
@@ -29,6 +28,7 @@ def cases_plugin_route():
 def create_case_route():
     """Create case default view"""
     form = CreateCase(request.form)
+    group_info = Groups.query.all()
     if request.method == 'POST':
         form.detection_method.choices = AVAILABLE_CHOICES
         if form.validate():
@@ -38,13 +38,13 @@ def create_case_route():
                     detection_method_selection = items
             new_case = Cases(description=form.description.data, subject=form.subject.data,
                              created_by=current_user.id, case_status="New Issue",
-                             detection_method=detection_method_selection[1], group_access=form.group_access.data)
+                             detection_method=detection_method_selection[1], group_access=form.group_access.data,
+                             current_user_navbar=get_group_permission_navbar())
             db.session.add(new_case)
             db.session.commit()
             flash("The case has been created.")
             return redirect(url_for('tasks.cases_plugin_route'))
-    group_info = Groups.query.all()
-    current_user_groups = get_groups()
+
     return render_template('create_case.html', title='Create Case', form=form, groups=group_info,
-                           render_current_user_groups=current_user_groups)
+                           current_user_navbar=get_group_permission_navbar())
 
