e383d97135edb436123b277f5414ee86382fbbf5
Wyatt Roersma <wyattroersma@gmail.com>
2018-05-22 03:41:31 -0600

    Update Horatio to use multipart for to allow files to be uploaded with it fix drop down menu select to return value so we can store it in the DB

--- forms.py
+++ forms.py
@@ -4,15 +4,13 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextAreaField, SelectMultipleField, IntegerField
 from wtforms.validators import Length
 from flask_babel import lazy_gettext as _l
-
 from app.plugins.Horatio.globals import AVAILABLE_CHOICES
-# coding=utf-8
 
 
 class CreateCase(FlaskForm):
     """Crease Case Form."""
     subject = TextAreaField(_l('Subject'), validators=[Length(min=0, max=256)])
     description = TextAreaField(_l('Description'), validators=[Length(min=0, max=256)])
-    detection_method = SelectMultipleField('Detection Method', choices=AVAILABLE_CHOICES)
+    detection_method = SelectMultipleField('Detection data_val.valuedata_val.valueMethod', choices=AVAILABLE_CHOICES)
     group_access = IntegerField(_l('GroupAccess'))
     submit = SubmitField(_l('Create'))
--- routes.py
+++ routes.py
@@ -7,7 +7,9 @@ from app.plugins.tasks.routes import tasks_page
 from app.plugins.Horatio.forms import CreateCase
 from app.plugins.Horatio.globals import AVAILABLE_CHOICES
 from app.plugins.Horatio.models import Cases
-
+from app.plugins.analysis.routes import get_upload_file_hash
+from app.plugins.analysis.file.upload import allowed_file
+from werkzeug.utils import secure_filename
 
 cases = Blueprint('cases', __name__, template_folder='templates')
 
@@ -19,32 +21,37 @@ def cases_plugin_route():
     # TODO show current cases in the database
     page = request.args.get('page', 1, type=int)
     case_list = Cases.query.all()
-    return render_template('cases.html', title='Cases Plugin',
-                           page=page, case_list=case_list)
+    return render_template('cases.html', title='Cases Plugin', page=page, case_list=case_list)
 
 
 @tasks_page.route('/create', methods=['GET', 'POST'])
 @login_required
 def create_case_route():
     """Create case default view"""
-    form = CreateCase(request.form)
     group_info = Groups.query.all()
     if request.method == 'POST':
+        form = CreateCase(request.form)
+        if "group_name" in request.values:
+            group_access_value = request.values["group_name"]
+            group_access_value_id = Groups.query.filter_by(group_name=group_access_value).first()
+            form.group_access = group_access_value_id.id
         form.detection_method.choices = AVAILABLE_CHOICES
         if form.validate():
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file_hash = get_upload_file_hash(file)
             detection_method_selection = None
             for items in AVAILABLE_CHOICES:
                 if items[0] == form.detection_method.data[0]:
                     detection_method_selection = items
             new_case = Cases(description=form.description.data, subject=form.subject.data,
                              created_by=current_user.id, case_status="New Issue",
-                             detection_method=detection_method_selection[1], group_access=form.group_access.data
-                             )
+                             detection_method=detection_method_selection[1], group_access=group_access_value_id.id)
             db.session.add(new_case)
             db.session.commit()
             flash("The case has been created.")
             return redirect(url_for('tasks.cases_plugin_route'))
-
-    return render_template('create_case.html', title='Create Case', form=form, groups=group_info,
-                           )
+    form = CreateCase(request.form)
+    return render_template('create_case.html', title='Create Case', form=form, groups=group_info)
 
