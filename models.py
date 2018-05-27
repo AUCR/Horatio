5fbb74f95985e76f6fd5853f22094b6ade427f71
Wyatt Roersma <wyattroersma@gmail.com>
2018-05-27 04:03:03 -0600

    Add new dropdown_label option change case status to use tasks_states table

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ class Cases(db.Model):
     subject = db.Column(db.String(256))
     created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
     group_access = db.Column(db.Integer, db.ForeignKey('groups.id'))
-    case_status = db.Column(db.String(128))
+    case_status = db.Column(db.String(128), db.ForeignKey('task_states.task_state_name'))
 
     def __repr__(self):
         return '<Cases {}>'.format(self.case_name)
